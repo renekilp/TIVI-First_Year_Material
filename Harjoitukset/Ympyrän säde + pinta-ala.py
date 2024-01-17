4ca8144b5fce7afb1fdf0b8afb99955f13db818b
reneki <rene.kilpelainen@gmail.com>
2024-01-17 19:25:56 +0200

    created a new .py file

---
+++ Harjoitukset/Ympyrän säde + pinta-ala.py
@@ -0,0 +1,6 @@
+pi = 3.141592653589793
+
+user_input = input("Syötä ympyrän säde: \n")
+user_int = int(user_input)
+new_value = pi * user_int ** 2
+print("Ympyrän pinta-ala on \n" + str(new_value) )
