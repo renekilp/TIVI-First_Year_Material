57d3c394ddc3ef5053dadbafae1ee9f74aacddce
reneki <rene.kilpelainen@gmail.com>
2024-01-19 16:58:18 +0200

    created new .py files and tweaked some lines

--- Tehtävät (Moduulit 1 + 2)/Massa muunnons.py
+++ Tehtävät (Moduulit 1 + 2)/Massa muunnons.py
@@ -5,10 +5,10 @@ third = input("Syötä leivisköjen määrä: \n")
 luoti_weight = float(first)
 naula_weight = float(second)
 leivis_weight = float(third)
-
 massa = 13.3 * ((leivis_weight * 20 + naula_weight) * 32 + luoti_weight)
 
 print(f"Massa nykymittojen mukaan:\n{int(massa / 1000)} kilogrammaa ja {int(massa % 1000)} grammaa.")
+
 
 
 
