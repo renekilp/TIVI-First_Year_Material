b431130559a71a5f12fe0eacb5ce4a38847d3d64
reneki <rene.kilpelainen@gmail.com>
2024-01-19 15:38:07 +0200

    created new .py files and tweaked some lines

---
+++ Tehtävät (Moduulit 1 + 2)/Suorakulmion pinta ja piiri.py
@@ -0,0 +1,8 @@
+a = input("Syötä suorakulmion kanta: \n")
+b = input("Syötä suorakulmion korkeus: \n")
+kanta = int(a)
+korkeus = int(b)
+pinta = kanta * korkeus
+piiri = kanta * 4
+print(f"Suorakulmion pinta-ala on \n" + str(pinta) + "\n" + "Suorakulmion piiri on \n" + str(piiri))
+
---
+++ Tehtävät (Moduulit 1 + 2)/Tehtävä.py
@@ -0,0 +1,10 @@
+eka = input("Syötä kokonaisluku: \n")
+toka = input("Syötä kokonaisluku: \n")
+kolmas = input("Syötä kokonaisluku: \n")
+a = int(eka)
+b = int(toka)
+c = int(kolmas)
+summa = a + b + c
+tulo = a * b * c
+keski = a + b + c / 3
+print(f"Syöttämäsi lukujen summa: {summa}, tulo: {tulo} ja keskiarvo: {keski}")
