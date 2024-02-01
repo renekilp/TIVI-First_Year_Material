e267145965c6c4dde05908fcbb1e168110af3f55
reneki <rene.kilpelainen@gmail.com>
2024-02-01 13:19:34 +0200

    tweaked some lines

--- Moduuli 5/Tehtävä 2 - Luku input for rakenteella.py
+++ Moduuli 5/Tehtävä 2 - Luku input for rakenteella.py
@@ -2,10 +2,12 @@ number_list = []
 
 while True:
     number_str = input("Syötä numero:\n")
-    number = int(number_str)
-    number_list.append(number)
-    if number == "":
-        number_list.sort(reverse=True)
+    if number_str == "":
         break
+
+number = int(number_str)
+number_list.append(number)
+number_list.sort(reverse=True)
+
 print(number_list[:5])
  #  TEE LOPPUUN!
