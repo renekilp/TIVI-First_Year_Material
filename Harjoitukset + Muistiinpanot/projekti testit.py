8bb45fb9d67b22a239b72811bee81b609f3ce0d9
reneki <rene.kilpelainen@gmail.com>
2024-03-11 14:15:31 +0200

    tweked some lines

---
+++ Harjoitukset + Muistiinpanot/projekti testit.py
@@ -0,0 +1,10 @@
+print(f"""
+        {Fore.GREEN}Welcome to the help screen!
+        Here's a list of all available commands:
+
+        1. tutorial
+        2. player info
+        3. quit
+        4. credits
+
+        """)
