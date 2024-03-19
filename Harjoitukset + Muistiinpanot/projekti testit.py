08d2eec7058026ef31b30e38eb70b9380da2e152
reneki <rene.kilpelainen@gmail.com>
2024-03-19 08:59:46 +0200

    tweked some lines

--- Harjoitukset + Muistiinpanot/projekti testit.py
+++ Harjoitukset + Muistiinpanot/projekti testit.py
@@ -1,10 +1,21 @@
-print(f"""
-        {Fore.GREEN}Welcome to the help screen!
-        Here's a list of all available commands:
+import os
 
-        1. tutorial
-        2. player info
-        3. quit
-        4. credits
+def clear_terminal():
+    # For Windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+    # For other platforms (Linux, MacOS)
+    else:
+        _ = os.system('clear')
 
-        """)
+# Function to clear terminal using ANSI escape codes
+def clear_terminal_ansi():
+    print("\033[H\033[J")
+
+# Example usage:
+print("This is before clearing the terminal.")
+# Choose one of the following methods
+clear_terminal()
+# Or
+clear_terminal_ansi()
+print("This is after clearing the terminal.")
