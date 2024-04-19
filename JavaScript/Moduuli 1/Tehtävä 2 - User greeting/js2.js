1ce2032bfb1044734be0de53411d682473b820e8
reneki <rene.kilpelainen@gmail.com>
2024-04-19 11:25:29 +0300

    tweaked some lines

--- JavaScript/Moduuli 1/Tehtävä 2 - User greeting/js2.js
+++ JavaScript/Moduuli 1/Tehtävä 2 - User greeting/js2.js
@@ -3,6 +3,6 @@ const name = prompt('Syötä nimi:');
 console.log(name);
 
 const firstParagraphElement = document.querySelector('p');
-firstParagraphElement.textContent = `Moi ${name}`;
+firstParagraphElement.textContent = `Moi, ${name}`;
 
 
