09e49abd71d935567b118f70a37c75a78dead0c0
renekilp <rene.kilpelainen@gmail.com>
2024-12-08 16:39:51 +0200

    math do be mathing

--- Math/Funktioiden piirtäminen.py
+++ Math/Funktioiden piirtäminen.py
@@ -1,17 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# ---------------------------------------------------------
+
 x = np.linspace(-5, 5, 100)
-y = x**2
-z = 2*x-4
+y = -1.5 * x ** 2 - 3 * x + 7
+z = x ** 2 - 4 * x + 3
 
 ax = plt.subplot()
 
-ax.spines['left'].set_position(('data', 0))
-ax.spines['bottom'].set_position(('data', 0))
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none')
+ax.spines['vasen'].set_position(('data', 0))
+ax.spines['ala'].set_position(('data', 0))
+ax.spines['oikea'].set_color('none')
+ax.spines['ylä'].set_color('none')
 
-# smart mittasuhteet
+# mittasuhteet
 ax.set_aspect('equal')
 
 # x-akselin säätöä
@@ -19,11 +22,20 @@ ax.set_xticks(np.arange(-5, 5, 1))
 
 
 # Y:lle rajat, että kuvaajat näkyvät fiksummin
-plt.ylim([-7, 7])
+plt.ylim([-10, 10])
 
 # label tarvitsee tuon legend():n
-plt.plot(x, y, label="$y = x^2$")
-plt.plot(x, z, label="$y = 3x - 5$")
-plt.legend()
 
+# ----------------------------------------------------------
+
+plt.plot(x, y, label=r"$y = -1 \ frac {1} {2} x^2 - 3x + 7$")
+plt.plot(x, z, label=r"$y = x^2 - 4x + 3$")
+plt.legend()
+plt.show()
+
+# ----------------------------------------------------------
+
+plt.plot(x, y, label=r"$y = x^2$")
+plt.plot(x, z, label=r"$y = 3x - 5$")
+plt.legend()
 plt.show()
